29689806b437f2e921b796c4610c31c361c16989
Jadenjesusdragonic <84882653+Jadenjesusdragonic@users.noreply.github.com>
2022-06-08 11:25:48 -0700

    Update protoproject.py

--- protoproject.py
+++ protoproject.py
@@ -1,3 +1,4 @@
+from inspect import Parameter
 import pylab
 
 from pycbc import waveform
@@ -6,10 +7,16 @@ from gwpy.timeseries import TimeSeries
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-import numpy as np 
+
 import pandas as pd
 import math
 from bajes.obs.gw import Series
+import pycbc.psd
+
+from pycbc.waveform import get_fd_waveform
+from pycbc.filter import matched_filter
+from pycbc import psd
+
 
 
 #time of GW170817
@@ -26,6 +33,8 @@ H1_analysis_data = TimeSeries.fetch_open_data(
 
 t = H1_analysis_data.times
 strain = H1_analysis_data.value
+
+
 
 # set the data properties coherently
 seglen = 8           # duration of the segment [s]
@@ -47,7 +56,11 @@ data = h5py.File(fn,'r')
 #defining samples
 
 posterior_samples = data['IMRPhenomPv2NRT_highSpin_posterior']
+#print parameter names 
 
+pnames=posterior_samples.dtype.names
+print('\n\n List of Parameters:')
+print(pnames,'\n')
 #using pandas to set pnames
 
 pnames = posterior_samples.dtype.names
@@ -75,6 +88,16 @@ ind = pnames.index('lambda2')
 
 lambda2_samples = np.array([samp[ind] for samp in posterior_samples[()]])
 
+#defining distance parameter
+ind=pnames.index('luminosity_distance_Mpc')
+
+distance_samples = np.array([samp[ind] for samp in posterior_samples[()]])
+
+#inclination parameter
+ind=pnames.index('costheta_jn')
+
+inclination_samples = np.array([samp[ind] for samp in posterior_samples[()]])
+
 #GW170817 has array shape ~4000, chooses where in the array to start taking data
 arr_index = 2000
 
@@ -90,7 +113,7 @@ iterations= 10
 #make 4 subplots so all simulations could be grouped together
 #formatting all 4 subplots
 plt.style.use('seaborn-pastel')
-fig, (f1, f2, f3, f4) = plt.subplots(4, figsize=(8, 13))
+fig, (f1, f2, f3, f4, f5,f6,f7) = plt.subplots(7, figsize=(8, 13))
 
 #make simulations
 for num in range(iterations):
@@ -99,6 +122,8 @@ for num in range(iterations):
   m2= m2_samples[arr_index]
   lambda1= lambda1_samples[arr_index]
   lambda2= lambda2_samples[arr_index]
+  luminosity_distance_Mpc=distance_samples[arr_index]
+  costheta_jn=inclination_samples[arr_index]
 #NRPM approx is fickle and does not work if lambda values are lower than 350
 #experimenting have found that sometimes it would work if a lambda1 = ~200 and 
 #lambda2 = >1000 however, for ease of use, I just set the values for both lambda
@@ -109,8 +134,11 @@ for num in range(iterations):
     m2= m2_samples[arr_index]
     lambda1= lambda1_samples[arr_index]
     lambda2= lambda2_samples[arr_index]
+    luminosity_distance_Mpc=distance_samples[arr_index]
+    costheta_jn=inclination_samples[arr_index]
     
-    
+
+
   #calculate chirp mass and mass ratio based on values received from previous data
   chirp_mass = (math.pow((m1 * m2), (3/5))) / (math.pow((m1 + m2), (1/5)))
   q = m1 / m2
@@ -126,8 +154,8 @@ for num in range(iterations):
               's2z'        : 0.,      # secondary spin parameter, z component
               'lambda1'    : lambda1,    # primary tidal parameter 
               'lambda2'    : lambda2,    # secondary tidal parameter
-              'distance'   : 100.8114416513031,    # distance [Mpc]   
-              'iota'       : np.pi,   # inclination [rad]   
+              'distance'   : luminosity_distance_Mpc,    # distance [Mpc]   #this will change 100.8114416513031 for using samples
+              'iota'       : costheta_jn,   # inclination [rad]   #this will change to using samples instead fixed value np.pi
               'ra'         : 0.,     # right ascension [rad]
               'dec'        : 0.,   # declination [rad]
               'psi'        : 0.,      # polarization angle [rad]
@@ -171,7 +199,7 @@ for num in range(iterations):
   #iterates arr_index for next simulation
   arr_index = arr_index + 1
   
- 
+
 #formating our waveform graphs individually
 f1.set_title('Postmerger h+ Strain vs. Time', fontsize=22, loc='center')
 f1.set_xlabel('Duration [s]', fontsize=14)
@@ -199,7 +227,85 @@ f4.set_ylabel("Amplitude")
 f4.set_xlim(xmin=.0, xmax=0.02)
 f4.set_facecolor(c)
 
-plt.tight_layout()
 
+#SNR-------------------------------------------------------------------
+print(pycbc.psd.get_lalsim_psd_list())
+
+delta_f = 1.0 / 4
+flen = int(1024 / delta_f)
+low_frequency_cutoff = 5
+
+xvh, _ = waveform.get_fd_waveform(approximant="IMRPhenomD", 
+                                  mass1=m1_samples[arr_index],
+                                  mass2=m2_samples[arr_index],
+                                  distance=luminosity_distance_Mpc,
+                                  delta_f=1.0,
+                                  f_lower=low_frequency_cutoff)
+                      
+p1 = pycbc.psd.aLIGOZeroDetHighPower(flen, delta_f, low_frequency_cutoff)
+
+
+
+
+
+f5.loglog(xvh.sample_frequencies,abs(xvh), label='xvh')
+f5.loglog(p1.sample_frequencies, p1, label='LowPower')
+
+#f5.set_xlim(20,10000)
+
+def waveform_power(waveform_model):
+  return 4*np.abs(waveform_model)*np.abs(waveform_model)*waveform_model.sample_frequencies
+
+CosmicExplorerPSD=psd.from_string('CosmicExplorerP1600143',len(xvh),xvh.delta_f, low_freq_cutoff=low_frequency_cutoff)
+AdvancedLIGOPlusPSD=psd.from_string('aLIGOAPlusDesignSensitivityT1800042',len(xvh),xvh.delta_f,low_freq_cutoff=low_frequency_cutoff)
+
+max_freq=CosmicExplorerPSD.sample_frequencies[-1]
+max_freq_APlus=AdvancedLIGOPlusPSD.sample_frequencies[-1]
+print(max_freq, max_freq_APlus)
+
+f6.loglog(CosmicExplorerPSD.sample_frequencies,CosmicExplorerPSD,label='CosmicExplorer psd')
+f6.loglog(AdvancedLIGOPlusPSD.sample_frequencies,AdvancedLIGOPlusPSD,label='A+ psd')
+f6.loglog(xvh.sample_frequencies, waveform_power(xvh), label='gravitational wave model PSD')
+f6.legend()
+
+
+from pycbc.filter.matchedfilter import sigma
+reference_SNR = sigma(xvh, CosmicExplorerPSD,low_frequency_cutoff=low_frequency_cutoff, high_frequency_cutoff=max_freq)
+reference_SNR_Aplus = sigma(xvh, AdvancedLIGOPlusPSD,low_frequency_cutoff=low_frequency_cutoff, high_frequency_cutoff=max_freq)
+
+
+
+print ("SNR of reference signal is", reference_SNR, "compared to", reference_SNR_Aplus,"in Advanced LIGO Plus")
+print ("Cosmic Explorer gives SNR",reference_SNR/reference_SNR_Aplus ,"times greater." )
+#SNR-----------------------------------------------------------------------------
+from pycbc.waveform import get_td_waveform 
+from pycbc.waveform import td_approximants 
+from pycbc.types import TimeSeries 
+from pycbc import waveform 
+from astropy.cosmology import WMAP9 as cosmo
+
+DELTA_T= 1.0/4096 #sampling rate 
+FLOW= 15.0 #low frequency cut off for waveforms
+
+
+#Define waveform function
+def generate_3g_waveform(m1,m2,distance,inclination,lambda1,lambda2): 
+    hp, hc = get_td_waveform(approximant='TaylorT4', 
+                                 mass1=m1_samples[arr_index], 
+                                 mass2=m2_samples[arr_index], 
+                                 distance = luminosity_distance_Mpc, 
+                                 delta_t=DELTA_T, 
+                                 f_lower=FLOW,
+                                 inclination=costheta_jn,
+                                 lambda1=lambda1,
+                                 lambda2=lambda2) 
+    
+
+
+
+
+f7.plot(hp, hp, label='demo' )
+f7.plot(hp, hc, label='demo1')
+f7.plot(xvh.sample_frequencies, waveform_power(xvh), label='gravitational wave model PSD')
 #saves the images 
-fig.savefig('/Users/jadenjesus/Documents/Grad_project/f6.png')
+fig.savefig('/Users/jadenjesus/Documents/Grad_project/test10.png')
